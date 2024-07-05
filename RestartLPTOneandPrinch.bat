7e3c19443ee31a638773b4537fefd9834a6cab2b
PBCLSstaff <168011484+PBCLSstaff@users.noreply.github.com>
2024-07-05 08:56:46 -0400

    Add files via upload

---
+++ RestartLPTOneandPrinch.bat
@@ -0,0 +1,10 @@
+net stop "LPT:One Job Queue Engine"
+net stop lptclient
+net stop "Print Delivery Station"
+net stop princhcloudconnector
+
+net start "LPT:One Job Queue Engine"
+timeout /t 5
+net start lptclient
+net start "Print Delivery Station"
+net start princhcloudconnector
