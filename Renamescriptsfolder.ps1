d2d1849b409ba1dce83be43f3e5aa3a393f20d06
PBCLSstaff <168011484+PBCLSstaff@users.noreply.github.com>
2024-07-05 11:48:41 -0400

    Add files via upload

---
+++ Renamescriptsfolder.ps1
@@ -0,0 +1,11 @@
+﻿function RenameFolder
+{
+    param ($Path = "C:\scripts", $Newfoldername = "scripts.old")
+
+    #Rename the folder
+    Rename-Item "$Path" "$Newfoldername"
+
+    #Remove Reconnect Drives link from every user's desktop
+    Remove-Item C:\Users\*\Desktop\Reconnect*.lnk -Force
+}
+RenameFolder
