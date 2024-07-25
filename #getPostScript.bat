87ae9aef9778d1fa3bc5ccef4fa940beba1dfe94
PBCLSstaff <168011484+PBCLSstaff@users.noreply.github.com>
2024-07-25 14:13:04 -0400

    Add files via upload

---
+++ #getPostScript.bat
@@ -0,0 +1,23 @@
+@echo off
+cls
+
+rem Navigate to the root directory to avoid confusion
+cd\
+
+rem Use xcopy to copy the specific file from network to local directory
+xcopy "\\staff.pbclibrary.org\it-ftp\#Copy Admin Desktop.bat" "C:\SysAdmin\" /Y /C /H /R /K /O /X
+
+rem Check Status
+if errorlevel 1 (
+    echo Failed to copy file.
+) else (
+   echo File copied successfully.
+    
+	cd %C:\SysAdmin\%
+
+
+	call "#Copy Admin Desktop.bat"
+)
+
+rem End 
+exit
