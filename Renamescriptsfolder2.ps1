6f35dcf4c3ad04dfedbda8690bfbdf9b30cec224
PBCLSstaff <168011484+PBCLSstaff@users.noreply.github.com>
2024-07-29 11:30:37 -0400

    Update Renamescriptsfolder2.ps1

--- Renamescriptsfolder2.ps1
+++ Renamescriptsfolder2.ps1
@@ -3,10 +3,14 @@
     param ($Path = "C:\scripts", $Newfoldername = "scripts.old")
     
     #Rename the folder
-    #Rename-Item "$Path" "$Newfoldername"
-    
+    Rename-Item "$Path" "$Newfoldername"
+
+    #Remove the script file from startup for all users
     Remove-Item "C:\Users\*\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\*.bat*" -Force
-   
+
+    #Remove the script shortcut from startup for all users
+    Remove-Item "C:\Users\*\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\*shortcut*" -Force
+
     #Remove Reconnect Drives link from every user's desktop
     Remove-Item "C:\Users\*\Desktop\Reconnect Drives*" -Force
 }
