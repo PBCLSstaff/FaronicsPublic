5b74702d8b78c282192a7c0709f61574940cf2a7
PBCLSstaff <168011484+PBCLSstaff@users.noreply.github.com>
2024-08-23 08:38:35 -0400

    Update RestartLPTOneandPrinch.bat

--- RestartLPTOneandPrinch.bat
+++ RestartLPTOneandPrinch.bat
@@ -2,9 +2,11 @@ net stop "LPT:One Job Queue Engine"
 net stop lptclient
 net stop "Print Delivery Station"
 net stop "PrinchCloudConnector"
+net stop "PrinchSoftwareUpdater"
 
 net start "LPT:One Job Queue Engine"
 timeout /t 5
 net start lptclient
 net start "Print Delivery Station"
 net start "PrinchCloudConnector"
+net start "PrinchSoftwareUpdater"
